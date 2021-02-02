c8bda260e0db7595a59fe0ebe04cca7ec4fdff24
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2021-02-02 13:34:43 +0300

    Add files via upload

--- tpl.js
+++ tpl.js
@@ -1,82 +1,128 @@
 
 window.onload = function () {
-  /* window.setTimeout(chooseOrder,2000);*/
-var n1 = document.querySelector("ul.container");
-    
-    n1.addEventListener('click',onClick,false);
-    n1.addEventListener('transitionend',onTransitionEnd,false);
-    /*n1.style.webkitAnimationPlayState = "paused";*/
-}
-
-var flagsOfApp = {
-    runAnimation: false,
-    index:0 
+  /* window.setTimeout(chooseOrder,2000); */
+  const btnR = document.querySelector('section.clearBtn>div:nth-child(2)');
+  const btnL = document.querySelector('section.clearBtn>div:nth-child(1)');
+  const container = document.querySelector('ul.container');
+  /* an event for  leaf through */
+  btnR.addEventListener('click', onClickR, false);
+  btnL.addEventListener('click', onClickL, false);
+  /* an event for animation */
+  container.addEventListener('transitionend', onTransitionEnd, false);
+  /* add listeners for a touchscreen */
+  container.addEventListener('touchend', onTuchEnd, false);
+  container.addEventListener('touchstart', onTuchStart, false);
+  /* n1.style.webkitAnimationPlayState = "paused"; */
 };
 
-function onClick (evt) {
-    /*checking - is an animation running*/
-  if (!flagsOfApp['runAnimation']) {
-      /*lock events*/
-    flagsOfApp['runAnimation'] = true;
-      /*get a parent node*/
-    var root = document.querySelector("ul.container");
-    
-    var anotherNode = document.querySelector("ul.container>li:nth-child(3)");
-    var n2 = document.querySelector("ul.container>li:nth-child(4)");
-    /*swapping nodes*/
+/* a global object for animation */
+const flagsOfApp = {
+  runAnimation: false,
+  index: 0,
+  startX: 0,
+  startY: 0,
+  endX: 0,
+  endY: 0
+};
+
+function onTuchEnd (evt) {
+  flagsOfApp.endX = evt.changedTouches[0].clientX;
+  flagsOfApp.endY = evt.changedTouches[0].clientY;
+  console.log(`startX ${flagsOfApp.startX},startY ${flagsOfApp.startY} \n`);
+  console.log(`endX: ${flagsOfApp.endX},endY: ${flagsOfApp.endY}`);
+  /* making a descision */
+  let delta = flagsOfApp.endX - flagsOfApp.startX;
+  const sign = Math.sign(delta);
+  delta = Math.abs(flagsOfApp.endX - flagsOfApp.startX);
+  if ((delta > 30) && (sign > 0)) {
+    /* run mouse event handler */
+    onClickR();
+  } else if ((delta > 30) && (sign < 0)) {
+    /* run mouse event handler */
+    onClickL();
+  }
+}
+
+function onTuchStart (evt) {
+  /* save coords */
+  flagsOfApp.startX = evt.changedTouches[0].clientX;
+  flagsOfApp.startY = evt.changedTouches[0].clientY;
+}
+
+function onClickL () {
+  /* checking - is an animation running */
+  if (!flagsOfApp.runAnimation) {
+    /* lock events */
+    flagsOfApp.runAnimation = true;
+    /* get a parent node */
+    const root = document.querySelector('ul.container');
+    /* swapping nodes */
     swapOrder(root);
+    /* get a first and a last elemes */
+    const first = root.querySelector('.first');
+    /* pElem.appendChild(first); */
+    first.style.transform = 'translateX(-95vw)';
+    first.style.marginTop = '';
+  }
+}
+
+function onClickR () {
+  /* checking - is an animation running */
+  if (!flagsOfApp.runAnimation) {
+    /* lock events */
+    flagsOfApp.runAnimation = true;
+    /* get a parent node */
+    const root = document.querySelector('ul.container');
+    /* swapping nodes */
+    swapOrder(root);
+    /* get a first and a last elemes */
+    const first = root.querySelector('.first');
+    /* pElem.appendChild(first); */
+    first.style.transform = 'translateX(95vw)';
   }
 }
 
 function swapOrder (pElem) {
-    /**additional swap function*/
-    /*first - on the bottom*/
-    /*fourth - on the top*/
-    function swp (val) {
-        switch(val.getAttribute('class')){
-            case "first":
-             val.setAttribute('class',"second");
-                break;
-            case "second":
-             val.setAttribute('class','thrid');
-                break;
-            case "thrid":
-             val.setAttribute('class','forth');
-                break;
-            case 'forth':
-              val.setAttribute('class','first');
-                break;
-                default:
-        }
-        /*clear inline style*/
-        val.style.transform = '';
+  /** additional swap function */
+  /* first - on the bottom */
+  /* fourth - on the top */
+  function swp (val) {
+    switch (val.getAttribute('class')) {
+      case 'first':
+        val.setAttribute('class', 'second');
+        break;
+      case 'second':
+        val.setAttribute('class', 'thrid');
+        break;
+      case 'thrid':
+        val.setAttribute('class', 'forth');
+        break;
+      case 'forth':
+        val.setAttribute('class', 'first');
+        break;
+      default:
     }
-    
-    /*********************/
-    /*get children*/
-    var nodeList = pElem.children;
-    var subArray = Array.prototype.slice.call(nodeList);
-    subArray.forEach(swp);
-    /*get a first and a last elemes*/
-    var first = pElem.querySelector('.first');
-    /*pElem.appendChild(first);*/
-   first.style.transform = "translateX(95vw)";   
-     
-}
+    /* clear inline style */
+    val.style.transform = '';
+  }
 
+  /*********************/
+  /* get children */
+  const nodeList = pElem.children;
+  const subArray = Array.prototype.slice.call(nodeList);
+  subArray.forEach(swp);
+}
 
 function onTransitionEnd (evt) {
-    
-    var targetNode = evt.currentTarget;
-    var topNode = targetNode.querySelector(".first");
-    if( topNode.style.transform === '') {
-         topNode.style.zIndex = '';
-        /*unlock click events*/
-         flagsOfApp['runAnimation'] = false;
-         return;
-    }
-    topNode.style.transform = '';
-    topNode.style.zIndex = '-10';
+  const targetNode = evt.currentTarget;
+  const topNode = targetNode.querySelector('.first');
+  if (topNode.style.transform === '') {
+    topNode.style.zIndex = '';
+    topNode.style.marginTop = '';
+    /* unlock click events */
+    flagsOfApp.runAnimation = false;
+    return;
+  }
+  topNode.style.transform = '';
+  topNode.style.zIndex = '-10';
 }
-
-
